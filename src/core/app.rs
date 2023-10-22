db44d3e0b16277b1a92fa97477a5eb4d0cb1270a
AndrooFrowns <17219603+AndrooFrowns@users.noreply.github.com>
2023-10-22 05:21:29 -0400

    Set up logging with the tracing crate

--- src/core/app.rs
+++ src/core/app.rs
@@ -1,7 +1,21 @@
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
 pub trait App {
     fn run(&self) {
+        init_logging();
+
         loop {
+            info!("loop start");
             todo!("implement primary loop");
         }
     }
+}
+
+fn init_logging() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Setting default logging subscriber failed");
 }
